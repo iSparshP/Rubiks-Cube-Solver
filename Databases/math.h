5876f15244ee317b5283d01fdf7ef5dc68134f7b
Sparsh Prakash <sparsh.prakash03@gmail.com>
2024-04-04 16:17:51 +0530

    added math.h and math.cpp

---
+++ Databases/math.h
@@ -0,0 +1,16 @@
+//
+// Created by Sparsh Prakash on 02/11/23.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_MATH_H
+#define RUBIKS_CUBE_SOLVER_MATH_H
+
+
+#include <cstdint>
+
+uint32_t factorial(uint32_t n);
+uint32_t pick(uint32_t n, uint32_t k);
+uint32_t choose(uint32_t n, uint32_t k);
+
+
+#endif //RUBIKS_CUBE_SOLVER_MATH_H
